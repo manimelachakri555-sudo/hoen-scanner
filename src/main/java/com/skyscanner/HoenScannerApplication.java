e93141465673d36d939eac8bc64b4768928b511c
Manimela Naga Chakri <manimelachakri555@gmail.com>
2026-06-16 09:47:05 +0530

    Completed Task 2

--- src/main/java/com/skyscanner/HoenScannerApplication.java
+++ src/main/java/com/skyscanner/HoenScannerApplication.java
@@ -1,8 +1,14 @@
 package com.skyscanner;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.dropwizard.core.Application;
 import io.dropwizard.core.setup.Bootstrap;
 import io.dropwizard.core.setup.Environment;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HoenScannerApplication extends Application<HoenScannerConfiguration> {
 
@@ -21,8 +27,29 @@ public class HoenScannerApplication extends Application<HoenScannerConfiguration
     }
 
     @Override
-    public void run(final HoenScannerConfiguration configuration, final Environment environment) {
+    public void run(final HoenScannerConfiguration configuration,
+                    final Environment environment) throws Exception {
 
+        ObjectMapper mapper = new ObjectMapper();
+
+        InputStream hotelsStream =
+                getClass().getClassLoader().getResourceAsStream("hotels.json");
+
+        InputStream carsStream =
+                getClass().getClassLoader().getResourceAsStream("rental_cars.json");
+
+        List<SearchResult> searchResults = new ArrayList<>();
+
+        searchResults.addAll(
+                mapper.readValue(hotelsStream,
+                        new TypeReference<List<SearchResult>>() {})
+        );
+
+        searchResults.addAll(
+                mapper.readValue(carsStream,
+                        new TypeReference<List<SearchResult>>() {})
+        );
+
+        environment.jersey().register(new SearchResource(searchResults));
     }
-
 }
